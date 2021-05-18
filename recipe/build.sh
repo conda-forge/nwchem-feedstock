2920fc1916b595ccf9f32e1aa6c4378cf26eb2cc
Edoardo Aprà <edoardo.apra@gmail.com>
2021-05-18 15:16:01 -0700

    Update build.sh
    removed true exit code for compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -59,7 +59,7 @@ cd "$NWCHEM_TOP"/src
 make CC=${CC} _CC=${_CC} FC=${FC} _FC=${_FC}  DEPEND_CC=${CC} nwchem_config
 cat ${SRC_DIR}/src/config/nwchem_config.h
 make DEPEND_CC=${CC} CC=${CC}  _CC=${CC} 64_to_32 
-make CC=${CC} DEPEND_CC=${CC} _CC=${_CC} FC=${FC} _FC=${_FC} V=1 CFLAGS_FORGA="-fPIC -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" || true
+make CC=${CC} DEPEND_CC=${CC} _CC=${_CC} FC=${FC} _FC=${_FC} V=1 CFLAGS_FORGA="-fPIC -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" 
 cat $NWCHEM_TOP/src/tools/build/config.log
 
 #=================================================
