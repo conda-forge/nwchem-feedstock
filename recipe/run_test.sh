79f84fed51f6d95b2093405253a4bb41fe6292a0
Leopold Talirz <leopold.talirz@gmail.com>
2021-05-19 21:11:17 +0200

    reduce size of test files
    The conda package ships all source files needed for running the tests.
    Since the test suite is large (>500MB) we want to ship only those tests
    that are actually necessary.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -11,7 +11,8 @@ export NWCHEM_TARGET=""
 export MPIRUN_PATH=$PREFIX/bin/mpirun 
 # nwchem cannot deal with path lengths >255 characters
 #export NWCHEM_BASIS_LIBRARY=$PREFIX/share/nwchem/libraries/
-export NWCHEM_BASIS_LIBRARY=$SRC_DIR/src/basis/libraries/
+cp -r $PREFIX/share/nwchem/libraries/ $SRC_DIR
+export NWCHEM_BASIS_LIBRARY=$SRC_DIR/libraries
 
 cd $NWCHEM_TOP/QA
 ./doafewqmtests.mpi 2 | tee tests.log
