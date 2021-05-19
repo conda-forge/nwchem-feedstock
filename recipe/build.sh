8e2c93abe2ecea10019a004252ce0939546e74f0
Leopold Talirz <leopold.talirz@gmail.com>
2021-05-19 09:42:31 +0200

    minor cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -30,7 +30,6 @@ else
 fi
 
 
-#export NWCHEM_MODULES="all python nwxc"
 export NWCHEM_MODULES="all python"
 #faster build
 #export NWCHEM_MODULES="nwdft driver solvation hessian property vib"
@@ -62,8 +61,8 @@ ${FC} -v
 #
 make CC=${CC} _CC=${_CC} FC=${FC} _FC=${_FC}  DEPEND_CC=${CC} nwchem_config
 cat ${SRC_DIR}/src/config/nwchem_config.h
-make DEPEND_CC=${CC} CC=${CC}  _CC=${CC} 64_to_32 
-    make CC=${CC} DEPEND_CC=${CC} _CC=${_CC} FC=${FC} _FC=${_FC} V=1 CFLAGS_FORGA="-fPIC -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" 
+make DEPEND_CC=${CC} CC=${CC} _CC=${CC} 64_to_32 
+make DEPEND_CC=${CC} CC=${CC} _CC=${_CC} FC=${FC} _FC=${_FC} V=1 CFLAGS_FORGA="-fPIC -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" 
 
 #=================================================
 #=Install=NWChem
