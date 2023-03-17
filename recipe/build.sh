2e057476256209f6eaa4659b390e5528bb1cc5dc
edoapra <edoardo.apra@gmail.com>
2023-03-16 17:45:08 -0700

    SIMINT_MAXAM=4

--- recipe/build.sh
+++ recipe/build.sh
@@ -30,9 +30,9 @@ else
 fi
 
 
-export NWCHEM_MODULES="all python gwmol"
+#export NWCHEM_MODULES="all python gwmol"
 #faster build
-#export NWCHEM_MODULES="nwdft driver solvation"
+export NWCHEM_MODULES="nwdft driver solvation"
 export NWCHEM_LONG_PATHS=y
 export USE_NOFSCHECK=Y
 #export USE_NOIO=Y
@@ -53,6 +53,7 @@ export LIBXC_LIB="$PREFIX/lib"
 
 # https://github.com/simint-chem/simint-generator
 export USE_SIMINT=1
+export SIMINT_MAXAM=4
 build_arch=$(echo $CONDA_TOOLCHAIN_HOST | cut -d - -f 1)
 echo "build_arch is $build_arch"
 if [[ "$build_arch" == "x86_64" ]]; then
